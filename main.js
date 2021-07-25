d1d4254884e7e7f6ed0f18dec9ca65ff3367997d
js43o <50646827+js43o@users.noreply.github.com>
2021-07-25 17:15:10 +0900

    Add date/time and clock number marks
    날짜/시간 및 시계 숫자 표시 추가.

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
 const DAY_MILLISECOND = 1000 * 60 * 60 * 24;
-let hands = document.querySelector('.hand').children;
 
-function rotateElem(elem, deg) {
-    if (deg % 360 === 0) {
-        elem.classList.add('no_transition');
-        elem.style.transform = `rotate(0deg)`;
-        setTimeout(() => elem.classList.remove('no_transition'), 0);
-    } else {
-        elem.style.transform = `rotate(${deg}deg)`;
-    }
+let hands = document.querySelector('.hand').children;
+let date = document.querySelector('.date');
+let time = document.querySelector('.time');
+
+
+const rotateElem = (elem, deg) => {
+    elem.style.transform = `rotate(${deg}deg)`;
 }
 
-function updateHands() {
-    let now = new Date();
+const updateHands = now => {
     rotateElem(hands[0], now.getHours() % 12 * 30 + now.getMinutes() * 0.5);
     rotateElem(hands[1], now.getMinutes() * 6);
     rotateElem(hands[2], now.getSeconds() * 6);
 }
 
-function startClock() {
-    setInterval(updateHands, 1000);
+const updateTime = now => {
+    let dates = [ now.getFullYear(), now.getMonth() + 1, now.getDate() ];
+    date.textContent = dates.map(fillDigit).join('/');
+
+    let times = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+    time.textContent = times.map(fillDigit).join(':');
 }
+
+const fillDigit = str => {
+    if (str < 10) return '0' + str;
+    else return str;
+}
+
+const updateClock = () => {
+    let now = new Date();
+    updateHands(now);
+    updateTime(now);
+}
+
+const startClock = () => {
+    setInterval(updateClock, 1000);
+}
+
 
 async function testSharp() {
     async function* genArr() {
@@ -36,5 +53,5 @@ async function testSharp() {
     }
 }
 
-updateHands();
+updateClock();
 startClock();
